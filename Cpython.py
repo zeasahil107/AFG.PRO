ba3b6d0400d6b3460fcf8bf1cfebf89a6057586d
zeasahil107 <111688905+zeasahil107@users.noreply.github.com>
2023-01-14 07:26:16 +0430

    Update Cpython.py

--- Cpython.py
+++ Cpython.py
@@ -1,1 +1,14 @@
-
+import marshal
+try:
+    import requests
+    from concurrent.futures import ThreadPoolExecutor as ThreadPool
+    import mechanize
+    from requests.exceptions import ConnectionError
+except ModuleNotFoundError:
+    os.system('pip install mechanize requests futures==2 > /dev/null')
+    os.system('python Pro07.py')
+from bs4 import BeautifulSoup
+from datetime import date
+from datetime import datetime
+from time import sleep
+from time import sleep as waktu
