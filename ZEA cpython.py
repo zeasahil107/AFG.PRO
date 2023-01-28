d9cff7e5565347ec89ec8b1faa0ebe2035ca82f5
zeasahil107 <111688905+zeasahil107@users.noreply.github.com>
2023-01-28 12:38:20 +0430

    Update ZEA cpython.py

--- ZEA cpython.py
+++ ZEA cpython.py
@@ -1,1 +1,27 @@
-
+import os, platform, time, sys
+def xoss(z):
+    for e in z + '\n':
+        sys.stdout.write(e)
+        sys.stdout.flush()
+        time.sleep(0.06)
+xoss('\n\x1b[1;37m[●] Checking Update........✔️✔️');time.sleep(0.5)
+os.system("git pull")
+def Update():
+    exit('\033[1;31m[●] Commands On Update Please Wait For Update ❤ ')
+def Run():
+        bit = platform.architecture()[0]
+        if bit == '64bit':
+            xoss("\x1b[1;92m[●] Congratulations ! Your Device Support this Tools 🍼🙂")
+            xoss('\x1b[1;94m[●] Join My Group First 🎈')
+            os.system#('xdg-open https://facebook.com/groups/1431748223768752/')
+            from HYPER import refat
+            refat()
+        elif bit == '32bit':
+            xoss("\n\x1b[1;92m[●] Congratulations ! Your Device Support this Tools 🍼🙂")
+            xoss('\x1b[1;94m[●] Follow My Github First 🎈')
+            os.system('xdg-open https://github.com/zeasahil107')
+            from HYPER import refat
+            refat()
+        else:
+            exit('\033[1;31m[●] Connection & Network Error 🤕')
+Run()
